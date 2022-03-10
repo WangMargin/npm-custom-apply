47859b89e0ed554715c5be6afc1853d7bff49460
wangyc <wycmargin@163.com>
2022-03-11 00:19:48 +0800

    fix: 成功发布npm包，并使用

--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -2,12 +2,16 @@
   <div>
       <h1>home</h1>
       <a-button>HOME</a-button>
+      <wangyc></wangyc>
   </div>
 </template>
 
 <script>
+import wangyc from 'prc.wangyc'
 export default {
-
+  components: {
+    "wangyc": wangyc.hello
+  }
 }
 </script>
 
