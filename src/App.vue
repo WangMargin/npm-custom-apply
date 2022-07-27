e2c83add63083735d85c3d8aa814352298ef7503
WangMargin <wycmargin@163.com>
2022-07-28 02:05:16 +0800

    fix: 组件中文化

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,51 @@
 <template>
-  <div id="nav">
-    <router-view />
-  </div>
+  <a-config-provider :locale="zhCN">
+    <router-view></router-view>
+  </a-config-provider>
 </template>
+<script>
+import enUS from 'ant-design-vue/es/locale/en_US';
+import zhCN from 'ant-design-vue/es/locale/zh_CN';
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+import { defineComponent } from 'vue';
+dayjs.locale('en');
+export default defineComponent({
+  setup() {
+    return {
+      dayjs,
+      enUS,
+      zhCN,
+    };
+  },
+});
+</script>
+<style scoped>
+.site-config-provider-calendar-wrapper {
+  width: 319px;
+  border: 1px solid #d9d9d9;
+  border-radius: 2px;
+}
+.locale-components {
+  border-top: 1px solid #d9d9d9;
+  padding-top: 16px;
+}
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+.example {
+  margin: 16px 0;
+}
+
+.example > * {
+  margin-right: 8px;
+}
+
+.change-locale {
+  margin-bottom: 16px;
+}
+[data-theme='dark'] .locale-components {
+  border-top: 1px solid #303030;
+}
+[data-theme='dark'] .site-config-provider-calendar-wrapper {
+  border: 1px solid #303030;
 }
 </style>
--- src/views/sj/index.vue
+++ src/views/sj/index.vue
@@ -1,47 +1,134 @@
 <template>
-  <div>
-    <a-form ref="createRef" :rules="createRule" :model="createForm">
-      <a-row :gutter="[15, 15]">
-        <a-col :xs="24" :sm="24">
-          <a-form-item name="category" label="类型名称">
-            <a-input v-model:value="createForm.category" placeholder="请填写职务类型名称"></a-input>
-          </a-form-item>
-        </a-col>
-        <a-col :xs="24" :sm="24">
-          <a-form-item name="standard" label="时长标准">
-            <a-input-number
-              v-model:value="createForm.standard"
-              placeholder="请填写时长标准"
-              style="width: 100%"
-            ></a-input-number>
-          </a-form-item>
-        </a-col>
-        <a-col :xs="24" :sm="24">
-          <a-form-item name="desc" label="类型描述">
-            <a-input v-model:value="createForm.desc" placeholder="请填写职务类型描述"></a-input>
-          </a-form-item>
-        </a-col>
-        <a-col :xs="24" :sm="24">
-          <a-form-item name="comment" label="备注">
-            <a-textarea v-model:value="createForm.comment" placeholder="请填写备注信息" />
-          </a-form-item>
-        </a-col>
-      </a-row>
-    </a-form>
-    <div style="text-align: center; padding: 30px 0">
-      <a-button @click="handleCancel">取消</a-button>
-      <a-button type="primary" @click="handleOK" class="mg-l10" :loading="state.loading">
-        确定
-      </a-button>
-    </div>
+  <div class="suojing-box">
+    <a-card>
+      <a-divider>
+        报备数据统计
+      </a-divider>
+      <a-form ref="createRef" :rules="createRule" :model="createForm">
+        <a-row :gutter="[15, 15]">
+          <a-col :xs="24" :sm="24" :md="24" :lg="12">
+            <a-form-item name="name" label="姓名">
+              <a-input v-model:value="createForm.name" placeholder="请填写姓名"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="24" :lg="12">
+            <a-form-item name="idNo" label="身份证号">
+              <a-input v-model:value="createForm.idNo" placeholder="请填写身份证号"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="24" :lg="12">
+            <a-form-item name="phone" label="手机号">
+              <a-input v-model:value="createForm.phone" placeholder="请填写手机号"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="24" :lg="12">
+            <a-form-item name="temperature" label="体温">
+              <a-input v-model:value="createForm.temperature" placeholder="请填写体温"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="24" :lg="12">
+            <a-form-item name="sourceAddress" label="何地返乡">
+              <a-input v-model:value="createForm.idNo" placeholder="请填写身份证号"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="24" :lg="12">
+            <a-form-item name="externalCauses" label="在外事由">
+              <a-select
+                v-model:value="createForm.externalCauses"
+                placeholder="请填写在外事由"
+                block
+                :options="pageData.externalCauses"
+              ></a-select>
+            </a-form-item>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="24" :lg="12">
+            <a-form-item name="comeDate" label="返程日期">
+              <a-date-picker
+                v-model:value="createForm.comeDate"
+                block
+                style="width: 100%"
+                :format="'YYYY-MM-DD'"
+                placeholder="请选择返程日期"
+              />
+            </a-form-item>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="24" :lg="12">
+            <a-form-item name="idNo" label="身份证号">
+              <a-input v-model:value="createForm.idNo" placeholder="请填写身份证号"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="24" :lg="12">
+            <a-form-item name="idNo" label="身份证号">
+              <a-input v-model:value="createForm.idNo" placeholder="请填写身份证号"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="24" :lg="12">
+            <a-form-item name="idNo" label="身份证号">
+              <a-input v-model:value="createForm.idNo" placeholder="请填写身份证号"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="24" :lg="12">
+            <a-form-item name="idNo" label="身份证号">
+              <a-input v-model:value="createForm.idNo" placeholder="请填写身份证号"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="24" :lg="12">
+            <a-form-item name="idNo" label="身份证号">
+              <a-input v-model:value="createForm.idNo" placeholder="请填写身份证号"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="24" :lg="12">
+            <a-form-item name="idNo" label="身份证号">
+              <a-input v-model:value="createForm.idNo" placeholder="请填写身份证号"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="24" :lg="12">
+            <a-form-item name="idNo" label="身份证号">
+              <a-input v-model:value="createForm.idNo" placeholder="请填写身份证号"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="24" :lg="12">
+            <a-form-item name="idNo" label="身份证号">
+              <a-input v-model:value="createForm.idNo" placeholder="请填写身份证号"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="24" :lg="12">
+            <a-form-item name="idNo" label="身份证号">
+              <a-input v-model:value="createForm.idNo" placeholder="请填写身份证号"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="24" :lg="12">
+            <a-form-item name="idNo" label="身份证号">
+              <a-input v-model:value="createForm.idNo" placeholder="请填写身份证号"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="24" :lg="12">
+            <a-form-item name="idNo" label="身份证号">
+              <a-input v-model:value="createForm.idNo" placeholder="请填写身份证号"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="24" :lg="12">
+            <a-form-item name="idNo" label="身份证号">
+              <a-input v-model:value="createForm.idNo" placeholder="请填写身份证号"></a-input>
+            </a-form-item>
+          </a-col>
+        </a-row>
+      </a-form>
+      <div style="text-align: center; padding: 30px 0">
+        <a-button @click="handleCancel">取消</a-button>
+        <a-button type="primary" @click="handleOK" class="mg-l10" :loading="state.loading">
+          确定
+        </a-button>
+      </div>
+    </a-card>
   </div>
 </template>
 
 <script>
-import { reactive, ref, onMounted, inject } from 'vue';
+import { reactive, ref, onMounted, defineComponent } from 'vue';
 import { useRouter } from 'vue-router';
 // import { message, Modal } from 'ant-design-vue';
-export default {
+export default defineComponent({
   name: '编辑职务类型数据',
   components: {},
   props: {},
@@ -51,6 +138,15 @@ export default {
       loading: false,
       subject: undefined,
       flag: false,
+    });
+    const pageData = reactive({
+      externalCauses: [
+        { label: '务工', value: 1 },
+        { label: '求学', value: 2 },
+        { label: '旅游', value: 3 },
+        { label: '就医', value: 4 },
+        { label: '其他', value: 5 },
+      ],
     });
     const createRef = ref();
 
@@ -90,10 +186,27 @@ export default {
       ],
     });
     const createForm = reactive({
-      category: '', // 职务类型名称
-      standard: '', // 时长标准
-      desc: '', // 职务类型描述
-      comment: '', // 备注
+      name: '王宜涛',
+      idNo: '13042719900325691X',
+      phone: '13601358946',
+      temperature: '36.2',
+      sourceProvinceCode: '001',
+      sourceProvinceName: '河北省',
+      sourceCityCode: '002',
+      sourceCityName: '邯郸市',
+      sourceAreaCode: '003',
+      sourceAreaName: '磁县',
+      sourceDetail: '振兴路复原小区A栋501',
+      externalCauses: 1,
+      comeDate: undefined,
+      comeWay: '电动自行车',
+      firstCheck: 0,
+      firstCheckDate: '2022-07-26 12:03:12',
+      secondCheck: 0,
+      secondCheckDate: '2022-07-26 12:03:12',
+      healthCode: 1,
+      carNumber: 'DS7A07',
+      vaccination: 2,
     });
     onMounted(() => {});
 
@@ -114,6 +227,7 @@ export default {
     return {
       handleOK,
       handleCancel,
+      pageData,
       router,
       state,
       createRule,
@@ -121,7 +235,12 @@ export default {
       createRef,
     };
   },
-};
+});
 </script>
 
-<style scoped></style>
+<style scoped>
+.suojing-box {
+  max-width: 1000px;
+  margin: 20px auto;
+}
+</style>
