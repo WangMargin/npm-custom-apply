e96702ffc0b6cbb72bf7d824c23fc6f8b71da093
wangyc <wycmargin@163.com>
2022-07-30 23:20:40 +0800

    fix: 二级目录生成链接

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Canvas2 from '@/views/html/2canvas.vue';
 import suojing from '@/views/sj/index.vue';
@@ -46,7 +46,7 @@ const routes = [
 
 const router = createRouter({
   // base: '/ppp/',
-  history: createWebHistory('/ppp/'),
+  history: createWebHashHistory('/ppp/'),
   routes,
 });
 
