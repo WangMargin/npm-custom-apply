c00cd29ca98bf53096263d1ae02fa9fde0197742
WangMargin <wycmargin@163.com>
2022-08-01 17:17:22 +0800

    fix: 设置 分享登录页面 html

---
+++ src/views/EarthMap/html/js/common.js
@@ -0,0 +1,5 @@
+$(() =>{
+  $('#content-left').load('./modules/ContentLeft.html'); // 左侧内容展示
+  $('#content-center').load('./modules/ContentCenter.html'); // 中间内容展示
+  $('#content-right').load('./modules/ContentRight.html'); // 右侧内容展示
+})
